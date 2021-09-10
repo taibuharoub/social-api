0e23165caa3f8652d615e0a5f888c3312eaa51ad
Taibu Haroub <haroubtaibu@gmail.com>
2021-09-10 13:11:51 +0300

    Setup auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import userRoutes from "./users.js";
+import authRoutes from "./auth.js";
 export default (app) => {
   app.get("/", (req, res, next) => {
     res.status(200).json({ message: "Default Index.js" });
   });
   app.use("/api/users", userRoutes);
+  app.use("/api/auth", authRoutes);
 };
