691f3ccc798f453c156d71e0d028debcc965617a
Taibu Haroub <haroubtaibu@gmail.com>
2021-09-10 13:23:48 +0300

    Add user model

---
+++ models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followering: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+userSchema.plugin(uniqueValidator);
+export default mongoose.model("User", userSchema);
