28942af379be2c9736b71d31249cc332a8c03392
Taibu Haroub <haroubtaibu@gmail.com>
2021-09-10 13:01:14 +0300

    Setup users

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
+import userRoutes from "./users.js";
 export default (app) => {
   app.get("/", (req, res, next) => {
     res.status(200).json({ message: "Default Index.js" });
   });
+  app.use("/api/users", userRoutes);
 };
